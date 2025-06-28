f736d750e8a437550c8085211728735d4e4a055f
Vaibhavkadam304 <vaibhav.kadam20@vit.edu>
2025-06-28 12:06:44 +0530

    changes

--- android/app/src/main/java/com/zestyvendorapp/stripe/ConnectionTokenProvider.kt
+++ android/app/src/main/java/com/zestyvendorapp/stripe/ConnectionTokenProvider.kt
@@ -2,18 +2,12 @@ package com.zestyvendorapp.stripe
 
 import com.stripe.stripeterminal.external.callable.ConnectionTokenCallback
 import com.stripe.stripeterminal.external.callable.ConnectionTokenProvider
-import com.stripe.stripeterminal.external.models.ConnectionTokenException
 
 /**
  * Implements Stripe Terminal’s ConnectionTokenProvider by proxying to your ApiClient.
  */
 class ConnectionTokenProvider : ConnectionTokenProvider {
   override fun fetchConnectionToken(callback: ConnectionTokenCallback) {
-    try {
-      val token = ApiClient.createConnectionToken()
-      callback.onSuccess(token)
-    } catch (e: ConnectionTokenException) {
-      callback.onFailure(e)
-    }
+    ApiClient.getConnectionTokenAsync(callback)
   }
 }
